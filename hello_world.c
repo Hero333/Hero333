6079f5503a01916b320bd45f9b793b823cd1fe30
Hero333 <86078262+Hero333@users.noreply.github.com>
2021-07-01 12:34:53 -0700

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+// helloworld.c 
+// compile 
+#include <stdio.h>
+
+int main(){
+
+	printf("Hello world\n");
+	printf("Hola Mundo\n");
+	printf("0 1 2 3 4 5 6 7 8 9 A B C D E F G \n");
+	return 0;
+	
+}
